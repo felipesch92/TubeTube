81fff3e7a9d880cf6237790fc4b4cd15fe1bf0a6
Felipe Schmaedecke <felipe.sch92@gmail.com>
2021-01-25 11:04:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-from pytube import YouTube
-yt = YouTube('https://www.youtube.com/watch?v=yw6QQhTjcoc')
-ys = yt.streams.first()
-ys.download('./videos')
+from pytube import YouTube, Playlist
+
+
+def baixa_video(yt):
+    maior_res = 0
+    maior_streams = ''
+    for i in yt.streams.filter(file_extension='mp4'):
+        x = str(i.resolution).replace('p', '')
+        if x.isnumeric() and int(x) > maior_res:
+            maior_res = int(x)
+            maior_streams = i
+    print(f'Baixando o vídeo {maior_streams.title}')
+    maior_streams.download('./downloads')
+
+
+while True:
+    op = input('Video ou Playlist? [V/P] ').upper()
+    if op not in 'VP':
+        print('Apenas V ou P!')
+    else:
+        break
+url = input('URL: ')
+if op == 'V':
+    yt = YouTube(url)
+    baixa_video(yt)
+else:
+    p = Playlist(url)
+    for video in p.videos:
+        baixa_video(video)
